3d98494ab61a32c6e29e5d7f406e2bc595220c45
John <97927839+JohnKakasas@users.noreply.github.com>
2024-11-25 22:06:59 +0200

    Delete cinema directory

--- cinema/controller/CREATE TABLE seats.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE seats (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    showtime_id INT NOT NULL,
-    seat_number VARCHAR(10) NOT NULL,
-    is_reserved BOOLEAN DEFAULT FALSE,
-    FOREIGN KEY (showtime_id) REFERENCES showtimes(id) ON DELETE CASCADE
-);
